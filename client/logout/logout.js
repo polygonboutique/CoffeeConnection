2f6159357107d7fe2eefee2d9d24f43781731cbc
polygonboutique <sergej.schefer@web.de>
2014-05-11 22:16:41 +0200

    remove logout button, if user is not logged on

--- client/logout/logout.js
+++ client/logout/logout.js
@@ -1,3 +1,13 @@
+Template.logout.rendered = function(){
+    if(Session.get("user") == null){
+        $("#logout").remove();
+    }
+};
+
+Template.logout.created = function(){
+
+};
+
 Template.logout.events({
     /** Logout code **/
     'click #logout': function (e, tpl) {

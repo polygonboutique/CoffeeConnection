8bac2d6442b22819a051eb2652e126854d8e3741
polygonboutique <sergej.schefer@web.de>
2014-05-14 18:45:50 +0200

    navigation list

--- client/onepage/onepage.js
+++ client/onepage/onepage.js
@@ -1,6 +1,12 @@
 Template.onepage.rendered = function(){
     var list = pageList.list; // pagelist ready
     console.log(pageList.list);
+
+    /*
+    * [0] -> [1]
+    * [0] <- [1] -> [2]
+    * [1] <- [2] -> [3]
+    * */
 };
 
 Template.onepage.isLoggedIn = function () {
